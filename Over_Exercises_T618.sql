ed97593d618c01eaa4fb2bcfa1c04585d2bec916
BiancaTulio <121875061+BiancaTulio@users.noreply.github.com>
2023-01-05 15:31:38 +0100

    Update Over_Exercises_T618.sql

--- Over_Exercises_T618.sql
+++ Over_Exercises_T618.sql
@@ -3,17 +3,58 @@ USE
 GO
 
 --1
---Ta fram resp anst‰llds fˆrnamn, efternamn, job, grundlˆn och total lˆnesumma fˆr hela fˆretaget (skapa en enda query, anv‰nd over)SELECT	Firstname,	Lastname,	Job,	Salary,	SUM(Salary) OVER() AS 'TotalSalaries'FROM	Employee--2--Som ovan men r‰kna ‰ven ut hur stor procentuell del av den total lˆnesumman varje anst‰lld har. Avrunda procenten till en decimal
---(skapa en enda query, anv‰nd over)SELECT	Firstname,									Lastname,	Job,	Salary,	SUM(Salary) OVER() AS 'TotalSalaries',	ROUND(CAST((Salary * 100) AS float) / CAST((SUM(Salary) OVER()) AS float), 2) AS 'PercentOfTotal'FROM	Employee--3--Som ovan men r‰kna ut bade hur stor procentuell del av den total lˆnesumman samt hur stor procentuell del varje anst‰lld har inom
---sin job-titel. Avrunda procent till en decimal (skapa en enda query, anv‰nd over)SELECT	Firstname,	Lastname,	Job,	Salary,	SUM(Salary) OVER() AS 'TotalSalaries',	ROUND(CAST((Salary * 100) AS float) / CAST((SUM(Salary) OVER()) AS float), 2) AS 'PercentOfTotal',	SUM(Salary) OVER(PARTITION BY Job ORDER BY Job) AS 'TotalByPosition',	ROUND(CAST((Salary * 100) AS float) / CAST((SUM(Salary) OVER(PARTITION BY Job)) AS float), 2) AS 'PercentByPosition'FROM	Employee--4--Ta fram resp anst‰llds fˆrnamn, efternamn, grundlˆn, avdelningsnamn och r‰kna ut hur stor procentuell del av den total lˆnesumman 
---samt hur stor procentuell del varje anst‰lld har inom sin avdelning. Avrunda procent till en decimal (skapa en enda query, anv‰nd over)
+--Ta fram resp anst√§llds f√∂rnamn, efternamn, job, grundl√∂n och total l√∂nesumma f√∂r hela f√∂retaget (skapa en enda query, anv√§nd over)
+SELECT
+	Firstname,
+	Lastname,
+	Job,
+	Salary,
+	SUM(Salary) OVER() AS 'TotalSalaries'
+FROM
+	Employee
+
+
+--2
+--Som ovan men r√§kna √§ven ut hur stor procentuell del av den total l√∂nesumman varje anst√§lld har. Avrunda procenten till en decimal
+--(skapa en enda query, anv√§nd over)
+SELECT
+	Firstname,								
+	Lastname,
+	Job,
+	Salary,
+	SUM(Salary) OVER() AS 'TotalSalaries',
+	ROUND(CAST((Salary * 100) AS float) / CAST((SUM(Salary) OVER()) AS float), 2) AS 'PercentOfTotal'
+FROM
+	Employee
+
+
+--3
+--Som ovan men r√§kna ut bade hur stor procentuell del av den total l√∂nesumman samt hur stor procentuell del varje anst√§lld har inom
+--sin job-titel. Avrunda procent till en decimal (skapa en enda query, anv√§nd over)
+SELECT
+	Firstname,
+	Lastname,
+	Job,
+	Salary,
+	SUM(Salary) OVER() AS 'TotalSalaries',
+	ROUND(CAST((Salary * 100) AS float) / CAST((SUM(Salary) OVER()) AS float), 2) AS 'PercentOfTotal',
+	SUM(Salary) OVER(PARTITION BY Job ORDER BY Job) AS 'TotalByPosition',
+	ROUND(CAST((Salary * 100) AS float) / CAST((SUM(Salary) OVER(PARTITION BY Job)) AS float), 2) AS 'PercentByPosition'
+FROM
+	Employee
+
+
+--4
+--Ta fram resp anst√§llds f√∂rnamn, efternamn, grundl√∂n, avdelningsnamn och r√§kna ut hur stor procentuell del av den total l√∂nesumman 
+--samt hur stor procentuell del varje anst√§lld har inom sin avdelning. Avrunda procent till en decimal (skapa en enda query, anv√§nd over)
 SELECT
 	Firstname,
 	Lastname,
 	Salary,
 	DeptName,
 	SUM(Salary) OVER() AS 'TotalSalaries',
-	ROUND(CAST((Salary * 100) AS float) / CAST((SUM(Salary) OVER()) AS float), 2) AS 'PercentOfTotal',	SUM(Salary) OVER(PARTITION BY DeptName ORDER BY DeptName) AS 'TotalByDept',
+	ROUND(CAST((Salary * 100) AS float) / CAST((SUM(Salary) OVER()) AS float), 2) AS 'PercentOfTotal',
+	SUM(Salary) OVER(PARTITION BY DeptName ORDER BY DeptName) AS 'TotalByDept',
 	ROUND(CAST((Salary * 100) AS float) / CAST((SUM(Salary) OVER(PARTITION BY DeptName)) AS float), 2) AS 'PercentOfDept'
 FROM
 	Employee As E
@@ -21,4 +62,4 @@ INNER JOIN
 	Department AS D
 ON
 	E.DeptID = D.DeptID
-
+	
