1fcc21cdd9658b8d3727e83e6b9a200a678a7160
BiancaTulio <bianca.yarck@gmail.com>
2023-01-25 15:38:05 +0100

    Add file

--- Cell_Level_Encryption_Example.sql
+++ Cell_Level_Encryption_Example.sql
@@ -1,6 +1,8 @@
 
 
--- Cell-level encryption example
+-- Cell-level encryption with asymmetric and symmetric keys example
+USE T618
+GO
 
 -- Creates a database master key encrypted by password $Str0nGPa$$w0rd
 CREATE MASTER KEY ENCRYPTION BY PASSWORD = '$tr0nGPa$$w0rd'
@@ -76,6 +78,95 @@ VALUES
 	('Drew Brees','7896541234', 
 	EncryptByKey(Key_GUID('mySymmetricKey'),'7896541234'));
 
-
 -- Close the key again, otherwise the data can be decrypted again
 CLOSE SYMMETRIC KEY MySymmetricKey
+
+
+--------------------------------------------------------------------
+
+
+-- Cell-level encryption with certificate example
+USE	BikeStores_Staging
+GO
+
+-- First create the master key that's going to be used to encrypt the certificate
+CREATE MASTER KEY 
+ENCRYPTION BY PASSWORD = 'P455w0rd' 
+
+-- To check the key you created
+SELECT 
+	[name], 
+	algorithm_desc 
+FROM 
+	sys.symmetric_keys
+
+-- Create the certificate
+CREATE CERTIFICATE EncryptionCert 
+WITH SUBJECT = 'Certificate for column level encryption'
+
+-- To check the certificate you created (notice that the certificate was created using the master key)
+SELECT 
+	[name], 
+	pvt_key_encryption_type_desc
+FROM 
+	sys.certificates
+
+-- Now create a symmetric key using the certificate
+CREATE SYMMETRIC KEY EncryptionKey 
+WITH ALGORITHM = AES_256
+ENCRYPTION BY CERTIFICATE EncryptionCert
+
+-- Checking the symmetric keys again, should have two of them
+SELECT 
+	[name], 
+	algorithm_desc 
+FROM 
+	sys.symmetric_keys
+
+-- Now create the table with the data to be encrypted. All fields that are going to be enrypted must be of datatype VARBINARY(MAX)
+CREATE TABLE 
+	Logins(
+		ID INT IDENTITY (1,1),
+		StaffID INT,
+		Fullname VARCHAR(25),
+		Username VARCHAR(25),
+		[Password] VARBINARY(MAX))
+
+-- Open the key using the certificate
+OPEN SYMMETRIC KEY EncryptionKey 
+DECRYPTION BY CERTIFICATE EncryptionCert
+
+-- Insert data into the table, using the key to encrypt the sensitive information
+INSERT INTO 
+	Logins (StaffID, Fullname, Username, [Password])
+VALUES
+	(1, 'Fabiola Jackson', 'FAJA', ENCRYPTBYKEY(KEY_GUID('EncryptionKey'),'Password123')), 
+	(2, 'Mireya Copeland', 'MICO', ENCRYPTBYKEY(KEY_GUID('EncryptionKey'),'Password123')),
+	(3, 'Genna Serrano', 'GESE', ENCRYPTBYKEY(KEY_GUID('EncryptionKey'),'Password456'))
+
+-- Close the key
+CLOSE SYMMETRIC KEY EncryptionKey
+
+-- See how the encrypted data looks like in the table. The two first rows had the same value but got different cyphers
+SELECT * FROM Logins
+
+-- To decrypt the data, open the key again by decrypting it with the certificate
+OPEN SYMMETRIC KEY EncryptionKey 
+DECRYPTION BY CERTIFICATE EncryptionCert
+ 
+-- But just selecting the data isn't enough to see it
+SELECT * FROM Logins
+ 
+-- DECRYPTBYKEY returns a VARBINARY datatype, so in order to see the data it needs to be cast to it's native datatype
+SELECT
+	ID,
+	StaffID,
+	Fullname,
+	Username,
+	CAST(DECRYPTBYKEY([Password]) AS varchar) AS 'Password'
+FROM
+	Logins
+
+-- Don't forget to close the key again
+CLOSE SYMMETRIC KEY EncryptionKey
+
